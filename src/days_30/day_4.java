0c8cc9f2d298bbca59684663949ae88f247bb976
Abhigya <abhigya.aks2@gmail.com>
2024-02-04 13:43:45 +0530

    new program

---
+++ src/days_30/day_4.java
@@ -0,0 +1,20 @@
+package days_30;
+
+public class day_4 {
+    static int factorial(int n){
+        if(n==0)
+            return 1;
+        else
+            return (n)*(factorial(n-1));
+    }
+
+    public static void main(String[] args) {
+        int n;
+        int fact;
+        fact = factorial(0);
+        System.out.println(fact);
+    }
+
+}
+
+
