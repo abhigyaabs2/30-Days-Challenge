e2341e338faa398e9a54583856ac69d805636e30
Abhigya <abhigya.aks2@gmail.com>
2024-02-06 19:30:10 +0530

    New commit

---
+++ src/days_30/day_6.java
@@ -0,0 +1,11 @@
+package days_30;
+
+public class day_6 {
+    public static void main(String[] args) {
+        int [] arr = {2,4,6,8};
+        System.out.println("The elements in the array are:");
+        for(int i =0; i<arr.length;i++){
+            System.out.print(arr[i]+"\t");
+        }
+    }
+}
