c8bb52e38750d80468874e17fa848050b0ef713d
Abhigya <abhigya.aks2@gmail.com>
2024-02-23 01:48:13 +0530

    Recursive function to find fibonacchi series

---
+++ src/days_30/day_22.java
@@ -0,0 +1,23 @@
+package days_30;
+
+import java.util.Scanner;
+
+public class day_22 {
+    static int n1=0,n2=1,n3=0;
+    public static void Fibonacci(int count){
+        if(count>0){
+            n3 = n1 + n2;
+            n1 = n2;
+            n2 = n3;
+            System.out.print(" "+n3);
+            Fibonacci(count-1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print(n1+" "+n2);//printing 0 and 1
+        Fibonacci(n-2);//n-2 because 2 numbers are already printed
+    }
+}
